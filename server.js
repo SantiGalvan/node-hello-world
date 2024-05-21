c941d66ebe7e32ae0a77adb9cc92152717b28788
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-21 17:27:58 +0200

    added button and import bootstrap

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const quotes = require('./quotes');
 const server = http.createServer((req, res) => {
     const index = Math.floor(Math.random() * (quotes.length - 1));
 
+    if (req.url === '/favicon.ico') {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(`
     <!DOCTYPE html>
@@ -17,11 +23,17 @@ const server = http.createServer((req, res) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>node-hello-world</title>
+
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
 
     <body>
-        <h1>${quotes[index].paragraph}</h1>
-        <author>${quotes[index].author}</author>
+        <div class="container text-center">
+            <h1>${quotes[index].paragraph}</h1>
+            <p>${quotes[index].author}</p>
+            <a href="" class="btn btn-primary">Nuova frase</a>
+        </div>
     </body>
 
     </html>
