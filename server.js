2c1837a6f022cbd2e3500a9b8dea1d864c5fc662
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-21 15:43:44 +0200

    added server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 require('dotenv').config();
+const http = require("http");
+const port = process.env.PORT;
+const host = process.env.HOST;
+const html = process.env.HTML;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<h1>${html}</h1>`);
+});
+
+server.listen(port, host, () => {
+    console.log(`Server avviato su http://${host}:${port}`);
+});
