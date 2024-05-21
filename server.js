5da91daddaf9e63d7d4b50439efcf02e60c9709e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-21 16:36:42 +0200

    added html boilerplait and fixed index

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const port = process.env.PORT;
 const host = process.env.HOST;
 const html = process.env.HTML;
 const quotes = require('./quotes');
-const index = Math.floor(Math.random() * quotes.length) - 1;
 
 const server = http.createServer((req, res) => {
+    const index = Math.floor(Math.random() * (quotes.length - 1));
+
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(`<h1>${quotes[index].paragraph}</h1>
-    <author>${quotes[index].author}</author>`);
+    res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>node-hello-world</title>
+    </head>
+
+    <body>
+        <h1>${quotes[index].paragraph}</h1>
+        <author>${quotes[index].author}</author>
+    </body>
+
+    </html>
+    `);
 });
 
 server.listen(port, host, () => {
