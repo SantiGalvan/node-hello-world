61e0eddf87b9aabd4b18f9297a8a121f99e7a685
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-21 17:57:53 +0200

    added title

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const http = require("http");
 const port = process.env.PORT;
 const host = process.env.HOST;
-const html = process.env.HTML;
+const title = process.env.TITLE;
 const quotes = require('./quotes');
 
 // Funzione
@@ -29,7 +29,7 @@ const server = http.createServer((req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>node-hello-world</title>
+        <title>${title}</title>
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
